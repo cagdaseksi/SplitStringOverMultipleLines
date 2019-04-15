1340071493b603e9d61ffa06ccccafac0cca86b2
cagdaseksi <cagdas.eksi@hotmail.com>
2019-04-15 09:42:01 +0300

    Initial commit

---
+++ SplitStringOverMultipleLines/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SplitStringOverMultipleLines
+//
+//  Created by MAC on 15.04.2019.
+//  Copyright © 2019 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let text = """
+        this is some text
+        over multiple lines
+        """
+        
+        print(text)
+        
+        
+    }
+
+
+}
+
